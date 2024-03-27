c17b05389650d4e9bafa17e3d19a933ac9ed96aa
umohit350 <umohit350@gmail.com>
2024-03-27 23:09:15 +0530

    first Project done

---
+++ 07_Project/Project_1/display.js
@@ -0,0 +1,24 @@
+const buttons = document.querySelectorAll(".button");
+const body = document.querySelector("body");
+
+buttons.forEach(function (button) {
+  button.addEventListener("click", function (e) {
+    console.log(e);
+    console.log(e.target);
+    if (e.target.id === "grey") {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === "pink") {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === "purple") {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === "green") {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === "red") {
+      body.style.backgroundColor = e.target.id;
+    }
+  });
+});
